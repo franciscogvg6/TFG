6c70a268cd1c758ac5a4f620a09312a1d8b0f8d3
franciscogvg6 <garciadelavegafrancisco@gmail.com>
2023-02-22 18:10:12 +0100

    "Feat:Login + Registro con firebase"

--- app/src/main/java/com/example/tfg/MenuPrincipalActivity.java
+++ app/src/main/java/com/example/tfg/MenuPrincipalActivity.java
@@ -2,13 +2,37 @@ package com.example.tfg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
+
+    Button btn_cerrar_sesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        btn_cerrar_sesion = findViewById(R.id.button3);
+        btn_cerrar_sesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(MenuPrincipalActivity.this, "Sesión cerrada", Toast.LENGTH_SHORT).show();
+                irALogin();
+            }
+        });
+    }
+
+    private void irALogin(){
+        Intent i = new Intent(this, RegistrarActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
     }
 }
