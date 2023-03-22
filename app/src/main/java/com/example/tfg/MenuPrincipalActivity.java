3f9020b0717658c74268baf2c9ef2a7a1388487c
franciscogvg6 <garciadelavegafrancisco@gmail.com>
2023-03-22 19:37:53 +0100

    "Feat: Listar Categorias"

--- app/src/main/java/com/example/tfg/MenuPrincipalActivity.java
+++ app/src/main/java/com/example/tfg/MenuPrincipalActivity.java
@@ -1,6 +1,9 @@
 package com.example.tfg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,15 +12,31 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
 
     Button btn_cerrar_sesion;
 
+    private CategoriaAdapter cAdapter;
+    private RecyclerView cRecyclerView;
+    private ArrayList<Categoria> cCategoriasList = new ArrayList<>();
+
+    private DatabaseReference cDatabase;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+
 
         btn_cerrar_sesion = findViewById(R.id.button3);
         btn_cerrar_sesion.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +47,18 @@ public class MenuPrincipalActivity extends AppCompatActivity {
                 irALogin();
             }
         });
+
+        cRecyclerView = (RecyclerView) findViewById(R.id.rv);
+        cRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        cDatabase = FirebaseDatabase.getInstance().getReference();
+
+        getCategoriasFromBd();
+        //cAdapter.getItemCount();
+
+
+
+
     }
 
     private void irALogin(){
@@ -35,4 +66,34 @@ public class MenuPrincipalActivity extends AppCompatActivity {
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(i);
     }
+
+    private void getCategoriasFromBd(){
+
+        cDatabase.child("Categorias").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+
+                    cCategoriasList.clear();
+
+                    for(DataSnapshot ds : dataSnapshot.getChildren()){
+                        String categoria = ds.child("categoria").getValue().toString();
+                        cCategoriasList.add(new Categoria(categoria));
+                    }
+
+                    cAdapter = new CategoriaAdapter(cCategoriasList, R.layout.rv_render);
+                    cRecyclerView.setAdapter(cAdapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/example/tfg/RegistrarActivity.java
@@ -0,0 +1,200 @@
+package com.example.tfg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.jetbrains.annotations.NotNull;
+
+public class RegistrarActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button btn_registrar, btn_login;
+
+    FirebaseAuth firebaseAuth;
+    AwesomeValidation awesomeValidation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        FirebaseAuth mAuth= FirebaseAuth.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user !=null){
+            irAHome();
+        }
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+        awesomeValidation.addValidation(this, R.id.editTextTextPersonName, Patterns.EMAIL_ADDRESS, R.string.invalid_mail);
+        awesomeValidation.addValidation(this, R.id.editTextTextPassword, ".{6,}", R.string.invalid_password);
+
+
+        email = findViewById(R.id.editTextTextPersonName);
+        password = findViewById(R.id.editTextTextPassword);
+        btn_registrar = findViewById(R.id.button);
+        btn_login = findViewById(R.id.button2);
+
+
+        btn_registrar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String mail = email.getText().toString();
+                String pass = password.getText().toString();
+
+                if(awesomeValidation.validate()){
+                    firebaseAuth.createUserWithEmailAndPassword(mail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegistrarActivity.this, "Usuario creado", Toast.LENGTH_SHORT).show();
+                                irAHome();
+
+
+                            }else {
+                                String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
+                                dameToastdeerror(errorCode);
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(RegistrarActivity.this, "Completa todos los datos", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(awesomeValidation.validate()){
+
+                    String mail = email.getText().toString();
+                    String pass = password.getText().toString();
+
+                    firebaseAuth.signInWithEmailAndPassword(mail, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                irAHome();
+
+                            }else {
+                                String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
+                                dameToastdeerror(errorCode);
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+    }
+
+    private void irAHome(){
+        Intent i = new Intent(this, MenuPrincipalActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+    }
+
+    private void dameToastdeerror(@NotNull String error) {
+
+        switch (error) {
+
+            case "ERROR_INVALID_CUSTOM_TOKEN":
+                Toast.makeText(RegistrarActivity.this, "El formato del token personalizado es incorrecto. Por favor revise la documentación", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_CUSTOM_TOKEN_MISMATCH":
+                Toast.makeText(RegistrarActivity.this, "El token personalizado corresponde a una audiencia diferente.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_INVALID_CREDENTIAL":
+                Toast.makeText(RegistrarActivity.this, "La credencial de autenticación proporcionada tiene un formato incorrecto o ha caducado.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_INVALID_EMAIL":
+                Toast.makeText(RegistrarActivity.this, "La dirección de correo electrónico está mal formateada.", Toast.LENGTH_LONG).show();
+                email.setError("La dirección de correo electrónico está mal formateada.");
+                email.requestFocus();
+                break;
+
+            case "ERROR_WRONG_PASSWORD":
+                Toast.makeText(RegistrarActivity.this, "La contraseña no es válida o el usuario no tiene contraseña.", Toast.LENGTH_LONG).show();
+                password.setError("la contraseña es incorrecta ");
+                password.requestFocus();
+                password.setText("");
+                break;
+
+            case "ERROR_USER_MISMATCH":
+                Toast.makeText(RegistrarActivity.this, "Las credenciales proporcionadas no corresponden al usuario que inició sesión anteriormente..", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_REQUIRES_RECENT_LOGIN":
+                Toast.makeText(RegistrarActivity.this,"Esta operación es sensible y requiere autenticación reciente. Inicie sesión nuevamente antes de volver a intentar esta solicitud.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
+                Toast.makeText(RegistrarActivity.this, "Ya existe una cuenta con la misma dirección de correo electrónico pero diferentes credenciales de inicio de sesión. Inicie sesión con un proveedor asociado a esta dirección de correo electrónico.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_EMAIL_ALREADY_IN_USE":
+                Toast.makeText(RegistrarActivity.this, "La dirección de correo electrónico ya está siendo utilizada por otra cuenta..   ", Toast.LENGTH_LONG).show();
+                email.setError("La dirección de correo electrónico ya está siendo utilizada por otra cuenta.");
+                email.requestFocus();
+                break;
+
+            case "ERROR_CREDENTIAL_ALREADY_IN_USE":
+                Toast.makeText(RegistrarActivity.this, "Esta credencial ya está asociada con una cuenta de usuario diferente.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_USER_DISABLED":
+                Toast.makeText(RegistrarActivity.this, "La cuenta de usuario ha sido inhabilitada por un administrador..", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_USER_TOKEN_EXPIRED":
+
+            case "ERROR_INVALID_USER_TOKEN":
+                Toast.makeText(RegistrarActivity.this, "La credencial del usuario ya no es válida. El usuario debe iniciar sesión nuevamente.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_USER_NOT_FOUND":
+                Toast.makeText(RegistrarActivity.this, "No hay ningún registro de usuario que corresponda a este identificador. Es posible que se haya eliminado al usuario.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_OPERATION_NOT_ALLOWED":
+                Toast.makeText(RegistrarActivity.this, "Esta operación no está permitida. Debes habilitar este servicio en la consola.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_WEAK_PASSWORD":
+                Toast.makeText(RegistrarActivity.this, "La contraseña proporcionada no es válida..", Toast.LENGTH_LONG).show();
+                password.setError("La contraseña no es válida, debe tener al menos 6 caracteres");
+                password.requestFocus();
+                break;
+
+        }
+
+    }
+
+
+
+}
