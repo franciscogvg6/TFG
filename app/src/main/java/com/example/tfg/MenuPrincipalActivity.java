4a257e030cc013a0e49baca230fd7a629afebef0
AYESA\fgarciaga <fgarciaga@ayesa.com>
2023-06-06 19:09:53 +0200

    fix: Categorias + feat: Perfil

--- app/src/main/java/com/example/tfg/MenuPrincipalActivity.java
+++ app/src/main/java/com/example/tfg/MenuPrincipalActivity.java
@@ -7,10 +7,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,6 +26,8 @@ public class MenuPrincipalActivity extends AppCompatActivity {
 
     Button btn_cerrar_sesion;
 
+    Button btn_perfil;
+
     private CategoriaAdapter cAdapter;
     private RecyclerView cRecyclerView;
     private ArrayList<Categoria> cCategoriasList = new ArrayList<>();
@@ -34,11 +38,13 @@ public class MenuPrincipalActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_menu_principal);
 
 
 
         btn_cerrar_sesion = findViewById(R.id.button3);
+        btn_perfil = findViewById(R.id.perfil);
         btn_cerrar_sesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,13 +54,23 @@ public class MenuPrincipalActivity extends AppCompatActivity {
             }
         });
 
-        cRecyclerView = (RecyclerView) findViewById(R.id.rv);
-        cRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        btn_perfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                irAPerfil();
+            }
+        });
+
+        cRecyclerView = findViewById(R.id.rv);
+        cRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        cAdapter = new CategoriaAdapter(cCategoriasList, R.layout.rv_render);
+        cRecyclerView.setAdapter(cAdapter);
 
         cDatabase = FirebaseDatabase.getInstance().getReference();
 
         getCategoriasFromBd();
-        //cAdapter.getItemCount();
+
 
 
 
@@ -67,24 +83,33 @@ public class MenuPrincipalActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+    private void irAPerfil(){
+        Intent i = new Intent(this, PerfilActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+    }
+
     private void getCategoriasFromBd(){
 
-        cDatabase.child("Categorias").addValueEventListener(new ValueEventListener() {
+        cDatabase.child("Categorias").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()){
+                cCategoriasList.clear();
 
-                    cCategoriasList.clear();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String categoria = ds.child("Nombre").getValue(String.class);
+                    cCategoriasList.add(new Categoria(categoria));
+                }
 
-                    for(DataSnapshot ds : dataSnapshot.getChildren()){
-                        String categoria = ds.child("categoria").getValue().toString();
-                        cCategoriasList.add(new Categoria(categoria));
-                    }
+                cAdapter = new CategoriaAdapter(cCategoriasList, R.layout.rv_render);
+                cRecyclerView.setAdapter(cAdapter);
 
-                    cAdapter = new CategoriaAdapter(cCategoriasList, R.layout.rv_render);
-                    cRecyclerView.setAdapter(cAdapter);
+                Log.d("FirebaseData", "Total categorias: " + cCategoriasList.size());
+                for (Categoria categoria : cCategoriasList) {
+                    Log.d("FirebaseData", "Categoria: " + categoria.getCategoria());
                 }
             }
+
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
@@ -94,6 +119,7 @@ public class MenuPrincipalActivity extends AppCompatActivity {
 
         });
     }
+
 
 
 }
