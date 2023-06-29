5d0695ad434b562069ed17310e8d65a5fdbf117a
AYESA\fgarciaga <fgarciaga@ayesa.com>
2023-06-29 22:23:33 +0200

    feat: Login Por Roles

--- app/src/main/java/com/example/tfg/ConfirmarOrdenActivity.java
+++ app/src/main/java/com/example/tfg/ConfirmarOrdenActivity.java
@@ -1,4 +1,14 @@
 package com.example.tfg;
 
-public class ConfirmarOrdenActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfirmarOrdenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmar_orden);
+    }
 }
